cdf7a5895d8eecf2020f46aeb0d9a734a97fe109
Brendon <brennon.mckeever@gmail.com>
2016-04-14 15:48:23 -0500

    Final working model

--- com/Brendon/Main.java
+++ com/Brendon/Main.java
@@ -2,7 +2,7 @@ package com.Brendon;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //main
 
         HVAC_MainGUI run = new HVAC_MainGUI();
         //FurnaceGUI furnanceRun = new FurnaceGUI();
